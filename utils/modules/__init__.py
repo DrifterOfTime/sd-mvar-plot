ad3634f7c8eb472ac5d0024d17dce9950ca95005
DrifterOfTime <jikannotorento@gmail.com>
2023-01-13 13:42:11 -0700

    Updated __init__.py's
    Now imports all modules from all files in `utils.modules`. Will split modules into different files going forward.

--- utils/modules/__init__.py
+++ utils/modules/__init__.py
@@ -1,2 +1,10 @@
-from ._IModule import _IModule
-from .modules import *
+from glob import glob
+from os import sep
+
+__modules = glob(f'utils{sep}modules{sep}*.py')
+
+for m in __modules:
+    if m[:2] != "__":
+        m = m.replace(sep, ".")
+        string = f'from {m} import *'
+        exec(string)
